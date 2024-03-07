39169734fb716e0ec97fdf478ee5c2e77e8bc0a7
reza1mahb <136892089+reza1mahb@users.noreply.github.com>
2024-03-07 10:07:29 +0330

    Update python_query.py

--- python_query.py
+++ python_query.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 
 def query_blob_hash(collection_name, blob_hash):
     # Connect to MongoDB
-    client = MongoClient('localhost', 27017)
+    client = MongoClient('localhost', 27987)
 
     # Access the database
     db = client['blob_storage']
